0ee29e8861922a9d759ef005c87dc896606489b1
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-09-04 08:27:36 +0530

    Add files via upload

---
+++ strings01.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    /* string str; */
+    /* cin>>str;
+    cout<<str; */
+    /* string str1(5, 'n');
+    cout<<str1<<endl; */
+    /* string str = "Jubin deori";
+    cout << str; */
+    string str;
+    getline(cin, str);
+    cout << str <<endl;
+
+    return 0;
+}
---
+++ strings03.cpp
@@ -0,0 +1,12 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string abc = "szxdcfvbhmkcfvgbhmk,";
+    abc.clear();
+    cout << abc;
+    return 0;
+}
