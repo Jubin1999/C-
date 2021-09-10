725978e22bb8ccca5d37866a254789aea219fc0e
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-09-10 12:30:30 +0530

    Add files via upload

---
+++ recursion24.cpp
@@ -0,0 +1,32 @@
+//Permutations
+//Given an array nums of distinct integers, return all the possible permutaions.You can return the answar in any order.
+
+#include "bits/stdc++.h"
+using namespace std;
+vector<vector<int>> ans;
+void permute(vector<int> &a, int idx){
+    if(idx == a.size()){
+        ans.push_back(a);
+        return;
+    }
+    for(int i = idx;i<a.size();i++){
+        swap(a[i],a[idx]);
+        permute(a,idx+1);
+        swap(a[i],a[idx]);
+    }
+    return;
+}
+int32_t main()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &i : a)
+        cin>>i;
+    permute(a,0);
+    for(auto v : ans){
+        for(auto i : v)
+            cout<<i<<" ";
+        cout<<"\n";
+    }     
+}
