2c72ef4aa2fc3ad840464b9132622884d7b2d2c0
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-10-03 01:51:43 +0530

    Add files via upload

---
+++ jub1234.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"My name is jubin deori from kolowlowa deori gaon"<<endl;
+    cout<<"My father name is khageswar deori";
+    return 0;
+}
