04289fae502a052f8cb0b2452dcfb6a08bbe8fe7
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-10-06 12:35:24 +0530

    Add files via upload

---
+++ jubin7002.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+
+    int n, i, add;
+    cin>>n>>i;
+    add = n+i;
+    cout<<"Addition: "<<n<<" + "<<i<<" = "<<add<<endl;
+    return 0;
+}
