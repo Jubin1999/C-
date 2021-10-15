bfe8f4c1f125426905edcab9f9a5fa5bc4a3af10
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-10-15 13:28:01 +0530

    Add files via upload

---
+++ xyz.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    char ch,cj,ck,cl,cg,add;
+    cin>>ch;
+    cin>>cj;
+    cin>>ck;
+    cin>>cl;
+    cin>>cg;
+    add=ch+cj+ck+cl+cg;
+    cout<<"The enter character is : "<<add<<endl;
+    return 0;
+}
