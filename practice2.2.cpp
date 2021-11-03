ffb543006587aa3c46e9f365021deb4e9c4e6de1
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-11-03 08:25:12 +0530

    Add files via upload

---
+++ practice2.2.cpp
@@ -0,0 +1,17 @@
+//Input/Output in C++
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int amount1;
+    cin>>amount1;
+
+    int amount2;
+    cin>>amount2;
+
+    int sum = amount1 + amount2;
+    cout<<"Sum\n";
+    cout<<sum;
+    return 0;
+}
