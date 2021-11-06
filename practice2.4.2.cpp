d199617e1fe67edccaf0b13b1dff951d3488e90f
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-11-06 12:08:36 +0530

    Add files via upload

---
+++ practice2.4.2.cpp
@@ -0,0 +1,27 @@
+/* #include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    while(n>0){
+        cout<<n<<endl;
+    }
+    return 0;
+} */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    while(n>0){
+        cout<<n<<endl;
+        cin>>n;
+    }
+    return 0;
+}
