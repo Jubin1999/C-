ebaddba9da5fce7ba1d17d9a1e0d8cf45ba57cb2
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-11-06 12:07:35 +0530

    Add files via upload

---
+++ practice2.4.cpp
@@ -0,0 +1,19 @@
+//Find sum of natural numbers till n.
+//Using (for)loop 
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int sum = 0;
+    for(int counter=1;counter<=n;counter++){
+        sum=sum+counter;
+    }
+
+    cout<<sum<<endl;
+
+    return 0;
+}
