4f6ce377c0649fae2fe2508168ce4ec4005fcafc
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-11-13 12:29:04 +0530

    Add files via upload

---
+++ practice6.3.cpp
@@ -0,0 +1,17 @@
+//Sum of first n natural number 
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int n){
+    int ans = 0;
+    for(int i=1; i<=n; i++)
+        ans += i;
+    return ans;
+}
+
+int32_t main(){
+    int n;
+    cin>>n;
+
+    cout<<sum(n)<<endl;
+}
